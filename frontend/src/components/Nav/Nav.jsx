2fcb57dcb3be7790da638ecdd3783466a0ffd8fd
BibhuDev <bibhuduttapanda7@gmail.com>
2026-07-18 12:49:51 +0530

    nav bar setup

---
+++ frontend/src/components/Nav/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <nav>
+        <h1>PORTFOLIO</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
