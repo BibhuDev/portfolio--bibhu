bb8355240c45370533892cead3157a12d5e7e77c
BibhuDev <bibhuduttapanda7@gmail.com>
2026-07-19 10:46:25 +0530

    navbar design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import Nav from "./components/Nav/Nav"
+import Home from "./components/Home/Home"
+import About from "./components/About/About"
 
 function App() {
   return(
     <>
       <Nav></Nav>
+      <Home></Home>
+      <About></About>
     </>
   )
 }
--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./Nav.css"
+import {Link} from "react-scroll"
 
 function Nav() {
+
+    let menu = useRef();
+
   return (
     <nav>
         <h1>PORTFOLIO</h1>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
+        <ul className='desktopMenu'>
+            <Link to="home" activeClass='active' spy={true} smooth={true} duration={400}><li>Home</li></Link>
+            <Link to="about" activeClass='active' spy={true} smooth={true} duration={400}><li>About</li></Link>
+            <Link to="projects" activeClass='active' spy={true} smooth={true} duration={400}><li>Projects</li></Link>
+            <Link to="contact" activeClass='active' spy={true} smooth={true} duration={400}><li>Contact</li></Link>
+        </ul>
+
+        <div className="hamburger" ref={menu} onClick={()=>{
+            
+        }}>
+            <div className="ham"></div>
+            <div className="ham"></div>
+            <div className="ham"></div>
+        </div>
+
+        <ul className='mobileMenu'>
+            <Link to="home" activeClass='active' spy={true} smooth={true} duration={400}><li>Home</li></Link>
+            <Link to="about" activeClass='active' spy={true} smooth={true} duration={400}><li>About</li></Link>
+            <Link to="projects" activeClass='active' spy={true} smooth={true} duration={400}><li>Projects</li></Link>
+            <Link to="contact" activeClass='active' spy={true} smooth={true} duration={400}><li>Contact</li></Link>
         </ul>
     </nav>
   )
