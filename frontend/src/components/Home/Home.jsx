a3a39959ac6d3432a4b1a0598534615c8fc082c3
BibhuDev <bibhuduttapanda7@gmail.com>
2026-07-20 11:55:43 +0530

    home page with animation and responsiveness

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import "./Home.css"
+import img from "../../assets/coding.jpeg"
+import { TypeAnimation } from "react-type-animation"
 
 function Home() {
   return (
     <div id="home">
-
+      <div className="lefthome">
+        <div className="homedetails">
+          <div className="line1">I'M</div>
+          <div className="line2">BIBHUDUTTA PANDA</div>
+          <div className="line3">
+            <TypeAnimation
+              sequence={[
+                "BACKEND DEVELOPER",
+                1500,
+                "AI DEVELOPER",
+                1500,
+                "SOFTWARE DEVELOPER",
+                1500,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </div>
+          <button>HIRE ME</button>
+        </div>
+      </div>
+      <div className="righthome">
+        <img src={img} alt="" />
+      </div>
     </div>
   )
 }
